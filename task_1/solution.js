6c0283e5b540610106fa6a35436b52b6c81196d7
Anastasia Polishchuk <a.polischuk98@gmail.com>
2023-08-18 01:26:27 +0300

    task 1 gotovo new

--- task_1/solution.js
+++ task_1/solution.js
@@ -4,24 +4,24 @@ function addInCartMessage(title, price) {
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
-    // создайте переменную message
-    let message;
-    // присвойте переменной message значение переменной productName
-    message = productName;
-    // добавьте в конец переменной message пробел
-    message += ' ';
-    // добавьте в конец переменной message текст 'за'
-    message += 'за';
-    // добавьте в конец переменной message пробел
-    message += ' ';
-    // добавьте в конец переменной message значение переменной productPrice
-    message += productPrice;
-    // добавьте в конец переменной message пробел
-    message += ' ';
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    message += 'теперь в корзине!';
-    // выведите переменную message в консоль
-    console.log(message);
+   // создайте переменную message
+   let message;
+   // присвойте переменной message значение переменной productName
+   message = productName;
+   // добавьте в конец переменной message пробел
+   message += ' ';
+   // добавьте в конец переменной message текст 'за'
+   message += 'за';
+   // добавьте в конец переменной message пробел
+   message += ' ';
+   // добавьте в конец переменной message значение переменной productPrice
+   message += productPrice;
+   // добавьте в конец переменной message пробел
+   message += ' ';
+   // добавьте в конец переменной message текст 'теперь в корзине!'
+   message += 'теперь в корзине!';
+   // выведите переменную message в консоль
+   console.log(message);
     // Конец решения задания №1.1.
 
     return message;
@@ -39,6 +39,7 @@ function addInCartCountChange(value) {
     // увеличьте значение newValue на 1
     newValue = newValue+1;
     // Конец решения задания №1.2.
+
     return newValue;
 }
 
@@ -53,9 +54,12 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     let newSumText = newSum + ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
-    newSumText = ${newSum} ₽;
+    newSumText = `${newSum} ₽`;
     // Конец решения задания №1.3.
     console.log(newSumText);
+
+    // Конец решения задания №1.3.
+
     return newSumText;
 }
 
